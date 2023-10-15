29b7b407aa7317b81c731b735ee0a18e96312fba
AlienIdeology <wcliaw610@gmail.com>
2023-10-15 16:36:42 -0500

    sidebar nav

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,37 @@
 import { 
     createBrowserRouter, 
-    createRoutesFromElements, 
-    RouterProvider, 
-    Route
+    RouterProvider
 } from 'react-router-dom'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Root from "./components/Root.jsx"
 import Main from "./components/pages/Main.jsx"
 import Profile from "./components/pages/Profile.jsx"
 import Settings from "./components/pages/Settings.jsx"
+import Login from "./components/pages/Login.jsx"
 import ErrorPage from "./components/pages/ErrorPage.jsx"
 
-const router = createBrowserRouter(
-    createRoutesFromElements(
-        <Route path="/" //element={<Nav />} 
-            errorElement={<ErrorPage />}>
-            <Route index element={<Main />}></Route>
-            <Route path="profile" element={<Profile />}></Route>
-            <Route path="settings" element={<Settings />}></Route>
-        </Route>
-    )
-);
-
-console.log(router);
+const router = createBrowserRouter([{
+        path: "/",
+        element: <Root />,
+        errorElement: <ErrorPage />,
+        children: [{
+            index: true,
+            element: <Main />
+        },{
+            path: "profile",
+            element: <Profile />
+        },{
+            path: "settings",
+            element: <Settings />
+        }, {
+            path: "login",
+            element: <Login />
+        }]
+}])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>,
+        <RouterProvider router={router} />
+    </React.StrictMode>
 )
