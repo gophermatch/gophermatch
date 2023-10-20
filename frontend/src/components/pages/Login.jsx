d5e5655aef614739714aa6d9421b342bfd2941d0
Whatshisname <120993368+Whatshisname303@users.noreply.github.com>
2023-10-19 20:08:12 -0500

    Add basic login page content

--- frontend/src/components/pages/Login.jsx
+++ frontend/src/components/pages/Login.jsx
@@ -1,7 +1,24 @@
+import '../../assets/css/login.css'
+import React from 'react'
+
 export default function Login() {
+    const [username, setUsername] = React.useState('')
+    const [password, setPassword] = React.useState('')
+    function onLoginAttempt() {
+        // do stuff probably
+    }
     return (
-        <div>
-            login
+        <div className="login-page">
+            <div className="login-container">
+                <h1>Gopher Match</h1>
+                <div className="login-form">
+                    <p>Username</p>
+                    <input type="text" value={username} onChange={() => setUsername(event.target.value)}/>
+                    <p>Password</p>
+                    <input type="text" value={password} onChange={() => setPassword(event.target.value)}/>
+                </div>
+                <button onClick={onLoginAttempt}>Login</button>
+            </div>
         </div>
     )
 }
