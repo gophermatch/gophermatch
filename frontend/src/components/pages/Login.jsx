9de389c3e25b871faba141c59c27436285db621b
Whatshisname <120993368+Whatshisname303@users.noreply.github.com>
2023-10-20 12:44:41 -0500

    Add login error messages

--- frontend/src/components/pages/Login.jsx
+++ frontend/src/components/pages/Login.jsx
@@ -4,20 +4,31 @@ import React from 'react'
 export default function Login() {
     const [username, setUsername] = React.useState('')
     const [password, setPassword] = React.useState('')
+    const [loginErr, setLoginErr] = React.useState('')
+
     function onLoginAttempt() {
-        // do stuff probably
+        let result = Math.random()
+        if (result < 0.2) {
+            // switch to match page?
+        } else if (result < 0.6) {
+            setLoginErr("Incorrect email or password")
+        } else {
+            setLoginErr("Server error please try again later")
+        }
     }
+
     return (
         <div className="login-page">
             <div className="login-container">
                 <h1>Gopher Match</h1>
                 <div className="login-form">
-                    <p>Username</p>
+                    <p>Email</p>
                     <input type="text" value={username} onChange={() => setUsername(event.target.value)}/>
                     <p>Password</p>
-                    <input type="text" value={password} onChange={() => setPassword(event.target.value)}/>
+                    <input type="password" value={password} onChange={() => setPassword(event.target.value)}/>
+                    <div className="login-failure">{loginErr}</div>
+                    <button onClick={onLoginAttempt}>Login</button>
                 </div>
-                <button onClick={onLoginAttempt}>Login</button>
             </div>
         </div>
     )
