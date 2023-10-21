8615e2c21e191e15f0fa514d6a9ec46d048e9345
AlienIdeology <wcliaw610@gmail.com>
2023-10-21 14:22:34 -0500

    session working

--- backend/src/routes/login.js
+++ backend/src/routes/login.js
@@ -4,22 +4,31 @@ const router = Router()
 
 // Login
 router.post('/', async (req, res) => {
-    let username = req.body.username;
-    let password = req.body.password;
+    let username = req.body.username
+    let password = req.body.password
+
+
+
+
+    req.session.username = username
 
     // store in db
-    console.warn(`Username: ${username} Hashed password: ${password}`);
-    res.json(`{username:${username}, Hashed password:${password}}`); // sends json to frontend
+    console.warn(`Username: ${username} Hashed password: ${password}`)
+    res.json(`{username:${username}, Hashed password:${password}}`) // sends json to frontend
 })
 
 // Logout
 router.delete('/', async (req, res) => {
     // let username = req.body.username;
-    let password = req.body.password;
+    let password = req.body.password
+    console.log(req.session)
+    req.session.destroy()
+    res.clearCookie('connect.sid')
 
     // load hash from db
     // let result = await bycrpt.compare(password, hash);
-    console.warn("Logout: Not yet implemented!");
+    console.warn("Logout: Not yet implemented!")
+    res.json(`username: ${req.session.username}`)
 })
 
 export default router
