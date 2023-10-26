b90aace26ab0e4960dfd4cf5d575175a6963c2a4
zachkaupp <zachkaupp@gmail.com>
2023-10-26 18:55:59 -0500

    add TODOs to /database profile.js

--- backend/src/database/profile.js
+++ backend/src/database/profile.js
@@ -1,3 +1,15 @@
 import { db, tableNames } from './db.js'
 import { queryRowsToArray, buildSelectString, buildInsertString } from './dbutils.js'
 
+// TODO: get name
+// getUserName(user_id, user_name)
+
+// TODO: create/update name
+// createUserName(user_id, user_name)
+
+
+// TODO: get bio
+// getUserBio(user_id, bio)
+
+// TODO: create/update bio
+// createUserBio(user_id, bio)
