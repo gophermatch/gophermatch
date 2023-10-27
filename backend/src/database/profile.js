4b6ffc963b5defce2fa787806d399e0461118471
AlienIdeology <wcliaw610@gmail.com>
2023-10-26 20:38:58 -0500

    backend profile template

--- backend/src/database/profile.js
+++ backend/src/database/profile.js
@@ -1,15 +1,40 @@
 import { db, tableNames } from './db.js'
 import { queryRowsToArray, buildSelectString, buildInsertString } from './dbutils.js'
 
-// TODO: get name
-// getUserName(user_id, user_name)
+// Returns profile object (with profile_name and bio)
+export async function getProfile(user_id) {
 
-// TODO: create/update name
-// createUserName(user_id, user_name)
+}
 
+// On second thought: Don't call this!! 
+// I will call this when we create user profile. That way all existing users automatically has an empty profile
+// creates and stores a profile in DB
+// second argument (profile obj) is optional
+export async function createProfile(user_id, profile) {
 
-// TODO: get bio
-// getUserBio(user_id, bio)
+}
 
-// TODO: create/update bio
-// createUserBio(user_id, bio)
+// updates the stored profile
+export async function updateProfile(user_id, profile) {
+
+}
+
+// Returns an array of "Questions and Answers" object
+// Could be an empty list if the user has no QnAs
+// Or throws an error if the user's profile hasn't been created
+export async function getQnAs(user_id) {
+
+}
+
+// Create an answer to a exisitng question
+// The question with question_id must exist
+// The user must have answered the question before
+export async function createQnA(user_id, question_id, answer) {
+
+}
+
+// Update an answer to a exisitng question
+// The question with question_id must exist
+export async function updateQnA(user_id, question_id, answer) {
+
+}
--- backend/src/routes/profile.js
+++ backend/src/routes/profile.js
@@ -2,5 +2,47 @@ import { Router } from 'express'
 import { createErrorObj } from './routeutil.js'
 const router = Router()
 
-
 export default router
+
+// Don't worry about completing all this. Prioritize create and get routes
+
+// GET api/profile/
+router.get('/', async (req, res) => {
+    const user_id = req.query.user_id
+})
+
+// Create profile
+// POST api/profile/
+router.post('/', async (req, res) => {
+    const user_id = req.body.user_id
+    const profile = req.body.profile
+})
+
+// Update profile
+// PUT api/profile/
+router.put('/', async (req, res) => {
+    const user_id = req.body.user_id
+    const profile = req.body.profile
+})
+
+// Get Question and Answers
+// GET api/profile/qna/
+router.get('/qna/', async (req, res) => {
+    const user_id = req.query.user_id
+})
+
+// Create Answer to Question
+// POST api/profile/qna/
+router.post('/qna/', async (req, res) => {
+    const user_id = req.body.user_id
+    const question_id = req.body.question_id
+    const answer = req.body.answer
+})
+
+// Update Answer to Question
+// POST api/profile/qna/
+router.put('/qna/', async (req, res) => {
+    const user_id = req.body.user_id
+    const question_id = req.body.question_id
+    const answer = req.body.answer
+})
