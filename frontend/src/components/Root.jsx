e826db53390b465c8902fd6bbc3c80b328f3121c
unknown <douir002@umn.edu>
2023-10-29 14:30:15 -0500

    Pushing side-bar

--- frontend/src/components/Root.jsx
+++ frontend/src/components/Root.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom"
 import Sidebar from "./Sidebar.jsx"
+import "../assets/css/sidebar.module.css"
 
 export default function Root() {
     return (
         <>
+        <body>
         <Sidebar />
         <Outlet />
+        </body>
         </>
     )
 }
