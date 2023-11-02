f5792a6330308d82661024cfc6540962f6031f34
AlienIdeology <wcliaw610@gmail.com>
2023-11-01 20:47:17 -0500

    add temporary code to show that user has logged in on profile page

--- frontend/src/components/pages/Profile.jsx
+++ frontend/src/components/pages/Profile.jsx
@@ -1,7 +1,10 @@
+import currentUser from '../../currentUser.js'
+import styles from '../../assets/css/profile.module.css'
+
 export default function Profile() {
     return (
-        <div>
-            profile stuff here
+        <div className={styles.test}>
+            {currentUser.logged_in ? currentUser.email+"'s" : ""} profile stuff here
         </div>
     )
 }
