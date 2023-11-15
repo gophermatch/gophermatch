16da02b531676079e27f6836170e6a0a33a09e18
Whatshisname <120993368+Whatshisname303@users.noreply.github.com>
2023-11-15 12:42:32 -0600

    Finish profile structural css

--- frontend/src/components/ui-components/Profile.jsx
+++ frontend/src/components/ui-components/Profile.jsx
@@ -1,10 +1,11 @@
 import Carousel from './Carousel'
 import styles from '../../assets/css/profile.module.css'
 import kanye from '../../assets/images/kanye.png'
+import other from '../../assets/images/testprofile.png'
 
 export default function Profile() {
 
-    let pictures = [kanye, kanye];
+    let pictures = [kanye, other, kanye];
 
 
     return (
@@ -14,16 +15,30 @@ export default function Profile() {
                     <Carousel pictures={pictures} />
                 </div>
                 <div className={styles.profileOptions}>
-                    Profile info here
+                    <p>profile item one</p>
+                    <p>profile item two</p>
+                    <p>profile item three</p>
+                    <p>profile item four</p>
+                    <p>profile item five</p>
+                    <p>profile item six</p>
                 </div>
             </div>
             <div className={styles.rightSide}>
                 <div className={styles.bioContainer}>
                     <p className={styles.name}>
-                        Input name here
+                        Kanye East
                     </p>
                     <p className={styles.bioText}>
-                        This is my bio, and there will be a lot more to come from me
+                        Hi, this is my bio. I'm just a lyrical
+                        genuis looking for some  guys who
+                        can match my flow putting out beats in
+                        the dorms. My favorite show is stranger
+                        things, and I like to just chill on a friday
+                        night with a boba tea and some pop tarts.
+                        Really living the life out here. You should
+                        match me if you're another cool gemini with
+                        a fire pair of Yeezys and a touch of swag.
+                        This year is gonna be lit, Ye out.
                     </p>
                 </div>
             </div>
