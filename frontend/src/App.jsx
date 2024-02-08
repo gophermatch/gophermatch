763e4fe724436ba49f445031d20489f9c9bd8f02
tnt417 <ar56058@gmail.com>
2024-02-07 22:53:50 -0600

    Implemented entire account creation frontend from figma

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,11 +11,14 @@ import Profile from "./components/pages/Profile.jsx"
 import Settings from "./components/pages/Settings.jsx"
 import Inbox from "./components/pages/Inbox.jsx";
 import Login from "./components/pages/Login.jsx"
+import AccountCreation from "./components/pages/AccountCreation.jsx"
 import ErrorPage from "./components/pages/ErrorPage.jsx"
 import currentUser from "./currentUser.js"
 import './assets/css/index.css'
 import Signup from './components/pages/Signup.jsx'
 import './index.css';
+
+
 
 // Redirects the main page "/" to login page if user is not logged in, 
 // or to match page if user is logged in
@@ -31,13 +34,27 @@ async function mainPageRedirect({request}) {
 // Redirects the login page to match page if user is logged in
 async function loginPageRedirect() {
     if (currentUser.logged_in) {
-        return redirect("/match")
+        if(currentUser.account_created) {
+            return redirect("/match")
+        }
+        return redirect("/account")
     }
     return null
 }
 
+// Redirects a page to match page if user has created an account
+async function accountCreatedRedirect() {
+    if(currentUser.account_created) {
+        return redirect("/match")
+    }
+    return null;
+}
+
 // Redirects a protected page to login page if user is not logged in
 async function unauthPageRedirect({request}) {
+    if(!currentUser.account_created){
+        return redirect("/account")
+    }
     if (!currentUser.logged_in) {
         let params = new URLSearchParams()
         params.set("from", new URL(request.url).pathname)
@@ -61,6 +78,10 @@ const router = createBrowserRouter([{
             path: "signup",
             element: <Signup />,
             loader: loginPageRedirect,
+        },{
+            path: "account",
+            element: <AccountCreation />,
+            loader: accountCreatedRedirect,
         },{
             // wrapper for pages that need sidebar
             path: "",
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,7 +7,27 @@ export default {
     "./src/components/**/**.jsx"
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'text-field': '0 0px 10px -3px rgba(0, 0, 0, 0.2), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+        'text-field-selected': '0 0px 10px 0px rgba(0, 0, 0, 0.4), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+      },
+      dropShadow: {
+        'button-hover': '0 0px 10px 0px rgba(0, 0, 0, 0.4)',
+      },
+      colors: {
+      maroon: 'rgb(118, 45, 45)',
+      red: 'rgb(255, 0, 0)',
+      white: 'rgb(255, 255, 255)',
+      offwhite: '#F2EBE6',
+      maroon_new: '#7D0000',
+      inactive_gray: '#C6C6C6',
+      },
+      fontFamily: {
+        lora: ['Lora', 'serif'],
+        inter: ['Inter', 'serif'],
+      },
+   },
   },
   plugins: [],
 }
