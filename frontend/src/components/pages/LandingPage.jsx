e3e14da02a10e1ca2ec2112f1613cd805966f555
AndrewLange1 <lang1395@umn.edu>
2024-02-12 14:30:53 -0600

    Made the landing page look similar to the slideshow. There are still a couple of small issues to fix

--- frontend/src/components/pages/LandingPage.jsx
+++ frontend/src/components/pages/LandingPage.jsx
@@ -4,23 +4,39 @@ import '../../output.css';
 
 const LandingPage = () => {
   return (
-    <div className="bg-gradient-to-r from-red-200 via-red-100 to-green-100 min-h-screen flex flex-col items-center size-full">
-      <header className="text-maroon mb-12 text-4x1 font-bold">
-        GopherMatch
-      </header>
-
-      <div className="flex justify-end m-4 space-x-4 absolute top-0 right-0">
-        <Link to="/landing/go-to-login" className="text-black">
-          Login
-        </Link>
-        <Link to="/landing/go-to-signup" className="text-black">
-          Signup
-        </Link>
+    <>
+      <div className="bg-maroon h-[3.25rem] justify-end space-x-4 mt-auto">
+        <div className="flex justify-end space-x-6 mr-4">
+          <Link to="/landing/go-to-login" className="text-doc text-lg mt-2 hover:text-yellow-500">
+            Login
+          </Link>
+          <Link to="/landing/go-to-signup" className="text-doc text-lg mt-2 hover:text-yellow-500">
+            Signup
+          </Link>
+        </div>
       </div>
-    </div>
+      <div className="bg-doc min-h-screen flex items-start px-4 ">
+        <header className="text-maroon mt-64 text-7xl font-lora">
+          <span className="font-bold ml-20">GopherMatch</span>
+          <p className="text-3xl mt-5 ml-20">University of Minnesota Roommate Finder</p>
+            <a href="/landing/go-to-signup" className="text-maroon flex items-center border">
+              <p className="text-xl text-maroon ml-20 mt-5 hover:text-yellow-500">Get started</p>
+              <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" className ="text-maroon mt-5 ml-2 hover:text-yellow-500">
+                <path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z" className = "fill-current"/>
+              </svg>
+            </a>
+        </header>
+        <div className="flex justify-start items-center h-screen flex-grow max-w-md">
+          <img src="/assets/images/logo.png" alt="Description of the image" className="w-[17rem] h-auto ml-20 mb-20" />
+        </div>
+      </div>
+      <div className="bg-maroon h-[3.25rem] justify-end space-x-4 mt-auto fixed bottom-0 left-0 w-full"></div>
+
+
+ 
+      
+    </>
   );
 };
-
-
 
 export default LandingPage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,16 @@
 module.exports = {
   content: ["./frontend/src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        maroon: 'rgb(118, 45, 45)',
+        red: 'rgb(255, 0, 0)',
+        white: 'rgb(255, 255, 255)',
+        doc: 'rgb(243, 237, 220)',
+      },
+      fontFamily: {
+        lora: ['Lora', 'serif'],
+      }
+    },
   },
-  plugins: [],
-}   
-
+}
