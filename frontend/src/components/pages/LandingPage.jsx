ec3e921f853f8cc1192de212a661be9fc92095a0
AndrewLange1 <lang1395@umn.edu>
2024-03-13 15:09:46 -0500

    Fixed small bugs in landing page such as border and text not highlighting

--- frontend/src/components/pages/LandingPage.jsx
+++ frontend/src/components/pages/LandingPage.jsx
@@ -6,10 +6,10 @@ const LandingPage = () => {
     <>
       <div className="bg-maroon h-[3.75rem] justify-end space-x-4 mt-auto">
         <div className="flex justify-end space-x-6 mr-4">
-          <Link to="/landing/go-to-login" className="text-doc text-lg mt-3 hover:text-yellow-500">
+          <Link to="/landing/go-to-login" className="text-doc text-lg mt-3 hover:text-gold">
             Login
           </Link>
-          <Link to="/landing/go-to-signup" className="text-doc text-lg mt-3 hover:text-yellow-500">
+          <Link to="/landing/go-to-signup" className="text-doc text-lg mt-3 hover:text-gold">
             Signup
           </Link>
         </div>
@@ -18,10 +18,10 @@ const LandingPage = () => {
         <header className="text-maroon mt-64 text-7xl font-lora">
           <span className="font-bold ml-20">GopherMatch</span>
           <p className="text-3xl mt-5 ml-20">University of Minnesota Roommate Finder</p>
-            <a href="/landing/go-to-signup" className="text-maroon flex items-center border">
-              <p className="text-xl text-maroon ml-20 mt-5 hover:text-yellow-500">Get started</p>
-              <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" className ="text-maroon mt-5 ml-2 hover:text-yellow-500">
-                <path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z" className = "fill-current"/>
+            <a href="/landing/go-to-signup" className="text-maroon flex items-center">
+              <p className="text-xl text-maroon ml-[5.2rem] mt-5 hover:text-gold">Get started</p>
+              <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" className="text-maroon mt-5 ml-2">
+                <path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z" fill="currentColor" className="hover:text-gold" />
               </svg>
             </a>
         </header>
