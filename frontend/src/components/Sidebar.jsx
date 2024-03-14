ae4d805de656d37253ce84ca498b4b5d3376e3c0
AndrewLange1 <lang1395@umn.edu>
2024-03-14 16:52:52 -0500

    Made the sidebar skinnier, aligned the text to the left, and made it larger. Also added an arrow to logout

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,27 +1,29 @@
-import { Link } from "react-router-dom"
-import Logout from "./Logout"
+import { Link } from "react-router-dom";
+import Logout from "./Logout";
 
 export default function Sidebar() {
     return (
         <>
-            <nav className="flex flex-col items-center p-16 w-full h-screen bg-maroon rounded-none shadow-md" id="nav">
+            <nav className="flex flex-col items-left text-left p-10 w-full text-2xl h-screen bg-maroon rounded-none shadow-md" id="nav">
                 <Link to="/" className="transform scale-100 transition-transform duration-200 hover:scale-110 active:scale-90">
-                    <img src="../assets/images/logo.png" className="sidebarLogo"></img>
+                    <img src="../assets/images/logo.png" className="sidebarLogo" alt="Logo" />
                 </Link>
                 <br></br>
-                <div className="">
+                <div className="text-left mt-10">
                     {[
                         ['Profile', '/profile'],
                         ['Settings', '/settings'],
                         ['Match', '/match'],
                         ['Inbox', '/inbox'],
                     ].map(([label, destination]) => (
-                        <Link to={destination} className="max-h-full text-white text-center flex-grow-10 flex justify-between flex-col mb-20 font-sans font-bold drop-shadow-lg transform scale-100 transition-transform duration-200 hover:scale-110 active:scale-90">{label}</Link>
+                        <Link key={label} to={destination} className="max-h-full text-white text-left flex-grow-10 flex justify-between flex-col mb-20 font-sans drop-shadow-lg transform scale-100 transition-transform duration-200 hover:scale-110 active:scale-90">{label}</Link>
                     ))}
 
-                    <Logout className="text-white font-bold transition-transform duration-200 hover:scale-110 cursor-pointer" />
+                    <div className="text-white transition-transform duration-200 hover:scale-110 cursor-pointer inline-flex items-center">
+                    <Logout id="logout" className="logout" /> <span className="ml-1" onClick={() => document.getElementById('logout').click()}>&#8594;</span>
+                    </div>
                 </div>
             </nav>
         </>
-    )
+    );
 }
