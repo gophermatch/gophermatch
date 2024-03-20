151e10f209c00b414e641b0ae8c19ea484542d4b
Adam Douiri <adamdouir@gmail.com>
2024-03-20 18:34:30 -0500

    updated carousel with hover on profile picture

--- frontend/src/components/ui-components/Carousel.jsx
+++ frontend/src/components/ui-components/Carousel.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import styles from '../../assets/css/carousel.module.css'
+import { useNavigate } from 'react-router-dom'
 
 export default function Carousel(props) {
     const [position, setPosition] = React.useState(0)
+    const [isHovering, setIsHovering] = useState(false);
+    const navigate = useNavigate();
+    const editable = props.editable;
 
     function shiftPosition(n) {
 
@@ -10,6 +14,18 @@ export default function Carousel(props) {
 
     const currentImage = props.pictures[position];
     const carouselLen = props.pictures.length
+
+    function showOverlay(){
+        if(editable){
+            setIsHovering(prev => !prev);
+        }
+    }
+
+    function gotoUpload(){
+        if(editable){
+            navigate("/inbox");
+        }
+    }
 
     let dots = props.pictures.map((pic, i) => {
         if (i == position) { // the key is weird, will need to change if issues
@@ -33,8 +49,11 @@ export default function Carousel(props) {
 
     return (
         <div className={styles.container}>
-            <div className={styles.imageWrapper}>
-                <img src={currentImage} />
+            <div class ="flex justify-center items-center">
+                {isHovering && <img src='../../assets/images/imageicon.png' class="absolute scale-[0.15]"/>}
+                <div id="imageWrapper" className = {"hover:bg-gray-900 hover:opacity-80 hover:blur-sm hover:h-full"}>
+                    <img src={currentImage} className = {"rounded-2xl"} onClick={gotoUpload} onMouseEnter={showOverlay} onMouseLeave={showOverlay}/>
+                </div>
             </div>
             {carouselLen > 1 && dotSection}
         </div>
