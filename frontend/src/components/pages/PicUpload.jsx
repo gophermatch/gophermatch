448224a88e3e70b305a7e14836df564ba643bc02
tnt417 <ar56058@gmail.com>
2024-03-27 13:19:34 -0500

    Fixed pic upload post-merge

--- frontend/src/components/pages/PicUpload.jsx
+++ frontend/src/components/pages/PicUpload.jsx
@@ -22,9 +22,11 @@ const PicUpload = () => {
             const response = await backend.get("/profile/user-pictures", {
                 params: {user_id: currentUser.user_id},
                 withCredentials: true,
-
             });
             if (response) {
+
+                console.log(response);
+
                 const data = response.data;
 
                 const newArray = [];
@@ -53,16 +55,18 @@ const PicUpload = () => {
             });
 
             setStatus('Profile pictured successfully removed!');
-            console.log(response.data);
+            console.log(response);
 
             console.log("Changing picture urls");
-            console.log(pictureUrls);
-
-            await fetchPictureUrls();
         } catch (error) {
             console.error('Error removing file:', error);
             setStatus('Failed to remove profile picture. Please try again later.');
         }
+
+        //TODO: might be bad practice, but fixes for now
+        await new Promise(resolve => setTimeout(resolve, 50));
+
+        await fetchPictureUrls();
     }
 
     const uploadFile = async (file, i) => {
@@ -118,37 +122,55 @@ const PicUpload = () => {
 
     return (
         <div className="h-screen w-screen bg-doc flex justify-center items-center">
-            <div className="bg-white rounded-[1.5rem] pt-[16rem] pb-[16rem] pl-[40rem] pr-[10rem] mr-[15rem] shadow-lg relative">
-                <div className="absolute bottom-0 left-0 w-full h-2/3 flex flex-col justify-center items-center cursor-pointer">
+            <div
+              className="bg-white rounded-[1.5rem] pt-[16rem] pb-[16rem] pl-[40rem] pr-[10rem] mr-[15rem] shadow-lg relative">
+                <div
+                  className="absolute bottom-0 left-0 w-full h-2/3 flex flex-col justify-center items-center cursor-pointer">
                     <input type="file" id="fileInput" className="hidden" onChange={handleFileChange} />
                     <label htmlFor="fileInput" className="cursor-pointer flex flex-col items-center w-full h-full">
                         <img
-                            src="https://www.svgrepo.com/show/344399/arrow-bar-up.svg"
-                            alt="Upload File"
-                            className="h-[12rem] w-[12rem] text-maroon mt-[3rem]"
+                          src="https://www.svgrepo.com/show/344399/arrow-bar-up.svg"
+                          alt="Upload File"
+                          className="h-[12rem] w-[12rem] text-maroon mt-[3rem]"
                         />
                         <p className="mt-2">Drag and drop or click to browse</p>
                         <p className={"mt-2 text-maroon_new"}>{status}</p>
                     </label>
                 </div>
                 <div className="absolute bottom-[calc(66.67%)] left-0 w-full h-[1px] bg-black"></div>
-                <Link to="/profile" className="absolute top-[3%] right-[3%] w-[8%] h-[5%] bg-maroon rounded-[1rem] cursor-pointer flex justify-center items-center"
-                    onClick={handleDoneClick}
+                <Link to="/profile"
+                      className="absolute top-[3%] right-[3%] w-[8%] h-[5%] bg-maroon rounded-[1rem] cursor-pointer flex justify-center items-center"
+                      onClick={handleDoneClick}
                 >
                     <span className="text-white">Done</span>
                 </Link>
-                {pictureUrls.map((url, index) => (
-                  <div key={index} className={`absolute top-[4%] left-[${20 + index * 20}%] w-[8rem] h-[8rem] bg-inactive_gray rounded-3xl flex justify-center items-center`}>
-                      {url && (
-                        <>
-                            <img src={url} alt={`Profile ${index + 1}`} className="w-[8rem] h-[8rem] object-cover rounded-3xl" />
-                            <button onClick={() => handleRemovePic(index)} className="absolute font-bold text-white bg-black bg-opacity-20 w-[25px] h-[25px] rounded-full transition duration-200 hover:bg-opacity-50">
-                                X
-                            </button>
-                        </>
-                      )}
-                  </div>
-                ))}
+                <div
+                  className="absolute top-[4%] left-[20%] w-[8rem] h-[8rem] bg-inactive_gray rounded-[1rem] flex justify-center items-center">
+                    {pictureUrls[0] && <img src={pictureUrls[0]} alt="Profile 1"
+                                            className="w-[8rem] h-[8rem] object-cover rounded-[1rem]" />}
+                    {pictureUrls[0] && <button onClick={() => handleRemovePic(0)}
+                                               className="absolute font-bold text-white bg-black bg-opacity-20 w-[25px] h-[25px] rounded-full transition duration-200 hover:bg-opacity-50">
+                        X
+                    </button>}
+                </div>
+                <div
+                  className="absolute top-[4%] left-[40%] w-[8rem] h-[8rem] bg-inactive_gray rounded-[1rem] flex justify-center items-center">
+                    {pictureUrls[1] && <img src={pictureUrls[1]} alt="Profile 2"
+                                            className="w-[8rem] h-[8rem] object-cover rounded-[1rem]" />}
+                    {pictureUrls[1] && <button onClick={() => handleRemovePic(1)}
+                                               className="absolute font-bold text-white bg-black bg-opacity-20 w-[25px] h-[25px] rounded-full transition duration-200 hover:bg-opacity-50">
+                        X
+                    </button>}
+                </div>
+                <div
+                  className="absolute top-[4%] left-[60%] w-[8rem] h-[8rem] bg-inactive_gray rounded-[1rem] flex justify-center items-center">
+                    {pictureUrls[2] && <img src={pictureUrls[2]} alt="Profile 3"
+                                            className="w-[8rem] h-[8rem] object-cover rounded-[1rem]" />}
+                    {pictureUrls[2] && <button onClick={() => handleRemovePic(2)}
+                                               className="absolute font-bold text-white bg-black bg-opacity-20 w-[25px] h-[25px] rounded-full transition duration-200 hover:bg-opacity-50">
+                        X
+                    </button>}
+                </div>
             </div>
         </div>
     );
