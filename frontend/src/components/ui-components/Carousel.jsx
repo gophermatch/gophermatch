647eb1dcdf7043a90d7ca525e784a33ea289ead6
Adam Douiri <adamdouir@gmail.com>
2024-03-26 21:31:26 -0500

    fixed carousel

--- frontend/src/components/ui-components/Carousel.jsx
+++ frontend/src/components/ui-components/Carousel.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom'
 import styles from '../../assets/css/carousel.module.css'
 
 export default function Carousel(props) {
     const [position, setPosition] = React.useState(0)
+    const [isHovering, setIsHovering] = useState(false);
+    const navigate = useNavigate();
+    const editable = props.editable;
+
+    function showOverlay(){
+        const imageWrapper = document.getElementById("imageWrapper");
+        if(editable){
+            if(imageWrapper.style.filter == "blur(2px)"){
+                imageWrapper.style.filter = "blur(0px)";
+                imageWrapper.style.opacity = 1;
+            } else {
+                imageWrapper.style.filter = "blur(2px)";
+                imageWrapper.style.opacity = 0.8;
+            }
+            setIsHovering(prev => !prev);
+        }
+    }
+
+    function gotoUpload(){
+        if(editable){
+            navigate("/PicUpload");
+        }
+    }
 
     function shiftPosition(n) {
 
@@ -33,8 +57,11 @@ export default function Carousel(props) {
 
     return (
         <div className={styles.container}>
-            <div className={styles.imageWrapper} class="border-[0.33vw] rounded-[1vw] border-maroon_new">
-                <img src={currentImage} />
+            <div class ="flex justify-center items-center">
+                {isHovering && <img src='../../assets/images/imageicon.png' class="absolute scale-[0.1]"/>}
+                <div id="imageWrapper" class="border-[0.33vw] rounded-[1vw] border-maroon_new">
+                    <img src={currentImage} className = {"rounded-[1vw"} onClick={gotoUpload} onMouseEnter={showOverlay} onMouseLeave={showOverlay}/>
+                </div>
             </div>
             {carouselLen > 1 && dotSection}
         </div>
