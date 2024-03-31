3452079d52f2f981e8c73017239d9f5b8598ac98
quuanng <quang.pham0824@gmail.com>
2024-03-31 02:50:22 -0500

    changed u_inboxt table to have seen columns

--- backend/src/database/schemas/u_inbox.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE u_inbox (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id_1 INT NOT NULL,
-    user_id_2 INT NOT NULL,
-    FOREIGN KEY (user_id_1) REFERENCES users(user_id),
-    FOREIGN KEY (user_id_2) REFERENCES users(user_id),
-    CONSTRAINT user_pair_unique UNIQUE (user_id_1, user_id_2),
-    CHECK (user_id_1 < user_id_2)
-);
