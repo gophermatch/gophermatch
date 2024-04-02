5e0b75daf3962ccf31be5fe473dcdbf996f4837b
AndrewLange1 <lang1395@umn.edu>
2024-04-01 22:27:08 -0500

    Finished settings ui with dropdown and improved saved changes button

--- frontend/src/components/pages/Settings.jsx
+++ frontend/src/components/pages/Settings.jsx
@@ -43,12 +43,12 @@ export default function Settings() {
                 return 'Date of Birth';
             case 'hometown':
                 return 'Hometown';
-            case 'housing_preference':
-                return 'Housing Preference';
-            case 'college':
-                return 'College';
             case 'major':
                 return 'Major';
+            case 'college':
+                return 'College';
+            case 'housing_preference':
+                return 'Housing Preference';
             case 'graduating_year':
                 return 'Graduation Year'
             case 'gender':
@@ -85,19 +85,26 @@ export default function Settings() {
         setEditMode(true);
     };
 
+    const colleges = ['CSE', 'CBS', 'Carlson', 'Design', 'CEHD', 'CFANS', 'Nursing', 'Other']
+    const gradYear = ['2025', '2026', '2027', '2028', '2029', 'Other']
+    const gender = ['Male', 'Female', 'Non-binary']
+    const housePref = ['Apartments', 'Dorms', 'Both']
+
     return (
         <div className="flex flex-col h-screen w-screen font-comfortaa bg-offwhite">
             <h1 className="ml-10 mt-10 font-lora text-4xl text-maroon">
                 {editMode ? 'Edit Profile' : 'Settings'}
             </h1>
             <div className="ml-10 mt-[4vh] h-1 w-[80vw] bg-maroon"></div>
-            {isEdited && (
-                <div className="ml-10 mt-[4vh] w-[80vw] mr-[1vw] flex flex-col justify-center items-center">
-                    <button className="bg-maroon text-doc rounded-full w-[10vw] h-[4vh] text-xl" onClick={handleSaveChangesClick}>
-                        Save Changes
-                    </button>
-                </div>
-            )}
+            <div className="ml-10 mt-[4vh] w-[80vw] mr-[1vw] flex flex-col justify-center items-center">
+                <button
+                    className={`text-doc rounded-full w-[10vw] h-[4vh] text-xl ${editMode ? 'bg-maroon' : 'bg-green-500'}`}
+                    onClick={handleSaveChangesClick}
+                    disabled={!isEdited}
+                >
+                    Save Changes
+                </button>
+            </div>
             <div className="flex flex-col w-full justify-center items-center text-maroon text-xl font-comfortaa">
                 <div className="flex flex-wrap w-[65vw] mr-[8vw] justify-center">
                     {userInfo.map(([key, value], index) => (
@@ -106,13 +113,67 @@ export default function Settings() {
                                 <p className="text-base mt-[3vh]">{getLabel(key)}</p>
                             </div>
                             <div className="flex justify-start items-center text-black text-xl">
-                            <input
-                                type="text"
-                                value={editMode && editKey === key ? editValues[key] : value}
-                                onChange={(e) => handleInputChange(key, e.target.value)}
-                                onClick={() => handleEditClick(key)} // Set editKey when clicking on an input field
-                                className="bg-gray-200 p-1 rounded-md"
-                            />
+                                {key === 'college' ? (
+                                    <select
+                                        value={editMode && editKey === key ? editValues[key] : value}
+                                        onChange={(e) => handleInputChange(key, e.target.value)}
+                                        onClick={() => handleEditClick(key)}
+                                        className="bg-gray-200 p-1 rounded-md w-full"
+                                    >
+                                        {colleges.map((college, index) => (
+                                            <option key={index} value={college}>{college}</option>
+                                        ))}
+                                    </select>
+                                ) : key === 'graduating_year' ? (
+                                    <select
+                                        value={editMode && editKey === key ? editValues[key] : value}
+                                        onChange={(e) => handleInputChange(key, e.target.value)}
+                                        onClick={() => handleEditClick(key)}
+                                        className="bg-gray-200 p-1 rounded-md w-full"
+                                    >
+                                        {gradYear.map((year, index) => (
+                                            <option key={index} value={year}>{year}</option>
+                                        ))}
+                                    </select>
+                                ) : key === 'gender' ? (
+                                    <select
+                                        value={editMode && editKey === key ? editValues[key] : value}
+                                        onChange={(e) => handleInputChange(key, e.target.value)}
+                                        onClick={() => handleEditClick(key)}
+                                        className="bg-gray-200 p-1 rounded-md w-full"
+                                    >
+                                        {gender.map((gender, index) => (
+                                            <option key={index} value={gender}>{gender}</option>
+                                        ))}
+                                    </select>
+                                ) : key === 'housing_preference' ? (
+                                    <select
+                                        value={editMode && editKey === key ? editValues[key] : value}
+                                        onChange={(e) => handleInputChange(key, e.target.value)}
+                                        onClick={() => handleEditClick(key)}
+                                        className="bg-gray-200 p-1 rounded-md w-full"
+                                    >
+                                        {housePref.map((pref, index) => (
+                                            <option key={index} value={pref}>{pref}</option>
+                                        ))}
+                                    </select>
+                                ) : key === 'major' ? (
+                                    <input
+                                        type="text"
+                                        value={editMode && editKey === key ? editValues[key] : value}
+                                        onChange={(e) => handleInputChange(key, e.target.value)}
+                                        onClick={() => handleEditClick(key)} // Set editKey when clicking on an input field
+                                        className="bg-gray-200 p-1 rounded-md w-full"
+                                    />
+                                ) : (
+                                    <input
+                                        type="text"
+                                        value={editMode && editKey === key ? editValues[key] : value}
+                                        onChange={(e) => handleInputChange(key, e.target.value)}
+                                        onClick={() => handleEditClick(key)} // Set editKey when clicking on an input field
+                                        className="bg-gray-200 p-1 rounded-md w-full"
+                                    />
+                                )}
                             </div>
                         </div>
                     ))}
