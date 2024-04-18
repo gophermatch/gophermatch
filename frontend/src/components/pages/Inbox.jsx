107c766798e067c8d2ff107de79a0790dc186938
AndrewLange1 <lang1395@umn.edu>
2024-04-17 22:55:15 -0500

    Fixing merge conflicts (I hope)

--- frontend/src/components/pages/Inbox.jsx
+++ frontend/src/components/pages/Inbox.jsx
@@ -67,6 +67,35 @@ export default function Inbox({ user_data }) {
         })();
     }, [updateDep]);
 
+    useEffect(() => {
+        fetchPictureUrls();
+    }, []);
+
+    const fetchPictureUrls = async () => {
+        console.log(user_id);
+        try {
+            if (!user_id) {
+                console.error("User ID is missing");
+                return;
+            }
+    
+            const response = await backend.get("/profile/user-pictures", {
+                params: {user_id: user_id},
+                withCredentials: true,
+            });
+            if (response && response.data) {
+                console.log("Picture URLs:", response.data.pictureUrls);
+                setPictureUrls(response.data.pictureUrls);
+            } else {
+                console.error("Failed to fetch picture URLs");
+            }
+        } catch (error) {
+            console.error("Error fetching picture URLs:", error);
+        }
+    };
+    
+    
+
     function unmatch(profileId) {
         backend.delete('/match/inbox-delete', {params: {
             user1_id: currentUser.user_id,
@@ -171,9 +200,13 @@ export default function Inbox({ user_data }) {
                                 </g>
                             </svg>
                         </div>
-                        <button className="text-[3vh]">{person.contact_email}</button>
-                        <button className="bg-maroon h-[6vh] z-10 w-[4vw] rounded-lg text-white text-[5vh]" onClick={() => unmatch(person.id)}>X</button>
-                    </div>
+                    </button>
+                    <button className="h-[14vh] w-[8vw] mt-[2vh] ml-[2vw] bg-maroon_transparent rounded-[1.5vh]"
+                        onClick={() => unmatch(person.user_id)}>
+                            <div style={{pointerEvents: 'none'}}>
+                                <svg fill="black" width="10vw" height="10vh" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="mt-[1.5vh] ml-[-1vw]"><path d="M1,20a1,1,0,0,0,1,1h8a1,1,0,0,0,0-2H3.071A7.011,7.011,0,0,1,10,13a5.044,5.044,0,1,0-3.377-1.337A9.01,9.01,0,0,0,1,20ZM10,5A3,3,0,1,1,7,8,3,3,0,0,1,10,5Zm12.707,9.707L20.414,17l2.293,2.293a1,1,0,1,1-1.414,1.414L19,18.414l-2.293,2.293a1,1,0,0,1-1.414-1.414L17.586,17l-2.293-2.293a1,1,0,0,1,1.414-1.414L19,15.586l2.293-2.293a1,1,0,0,1,1.414,1.414Z"/></svg>
+                            </div>
+                    </button>
                 </div>
             ))}
         </div>
