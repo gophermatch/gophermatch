9d0dd493c759dabc2917934bfeeddac91f42463c
RoyalOrangefire <adhianandaraj@gmail.com>
2024-04-24 20:28:18 -0500

    changed back

--- backend/src/routes/login.js
+++ backend/src/routes/login.js
@@ -12,35 +12,40 @@ const router = Router()
 // On successful login, sets a session corresponding with the user
 // login.js route adjustment for multiple logins
 router.put('/', async (req, res) => {
-    let email = req.body.email;
-    let password = req.body.password;
+    let email = req.body.email
+    let password = req.body.password
 
+    // Check if request includes email and password
     if (!email || !password) {
-        res.status(400).json(createErrorObj("Email or password field missing"));
-        return;
+        res.status(400).json(createErrorObj("Email or password field missing from request body"))
+        return
     }
 
-    // If a session exists but does not match the current login attempt, consider logging out the old session
-    if (req.session.user && req.session.user.email !== email) {
-        req.session.destroy(); // Destroy the existing session if it doesn't match
+    // User already logged in
+    // Prevent user from loggin in again (perhaps with a different account) at the same time
+    if (req.session.user) {
+        res.status(400).json(createErrorObj("Already logged in!"))
+        return
     }
 
+    // If user is already signed in, we update their session object in case their password has changed
     try {
-        const user = await getUser(email);
+        const user = await getUser(email)
+
+        // Check if password matches with the hashed password
         const match = await bcrypt.compare(password, user.hashpass);
         if (!match) {
-            res.status(400).json(createErrorObj("Incorrect email or password"));
-            return;
+            res.status(400).json(createErrorObj("Email or password is incorrect"))
+            return
         }
 
-        // Reset the session with the current user's details
-        loginUser(req, user);
-        res.status(200).json({ email: user.email, user_id: user.id });
+        const userWithoutPass = loginUser(req, user)
+        res.status(200).json(userWithoutPass)
     } catch(e) {
-        console.error(e);
-        res.status(400).json(createErrorObj("Login error occurred"));
+        console.error(e)
+        res.status(400).json(createErrorObj(e))
     }
-});
+})
 
 // Logout
 // No requirement for the request
