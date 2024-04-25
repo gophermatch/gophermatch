58a06c430371cceb55839dbc6085fa010afcdf57
RoyalOrangefire <adhianandaraj@gmail.com>
2024-04-24 19:18:44 -0500

    changed domain to cribby.me on server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -39,7 +39,7 @@ server.use(session({
 
 server.use(cors({
     // process.env.NODE_ENV === 'production' ? 'https://cribby.me' : 
-    origin: process.env.NODE_ENV === 'production' ? 'https://gopher-match-970e05644fb9.herokuapp.com' : 'http://localhost:8080',
+    origin: process.env.NODE_ENV === 'production' ? 'https://cribby.me' : 'http://localhost:8080',
     credentials: true
 }));
 if(process.env.NODE_ENV === 'production'){
