bd0d170b895c54927429ae3d02c889c260148d78
quuanng <quang.pham0824@gmail.com>
2024-05-10 13:28:33 -0500

    updated top five table

--- backend/src/database/schemas/u_topfive.sql
+++ backend/src/database/schemas/u_topfive.sql
@@ -3,5 +3,12 @@ CREATE TABLE u_topfive (
     option_id INT,
     input VARCHAR(255),
     PRIMARY KEY (user_id, option_id),
+    user_id INT PRIMARY KEY,
+    question VARCHAR(255),
+    input1 VARCHAR(255),
+    input2 VARCHAR(255),
+    input3 VARCHAR(255),
+    input4 VARCHAR(255),
+    input5 VARCHAR(255),
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
