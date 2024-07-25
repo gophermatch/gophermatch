45b388c819186a656d199f8c640d4655ee3aa946
Adam Douiri <adamdouir@gmail.com>
2024-07-25 18:00:45 -0500

    basic displays for the match page

--- frontend/src/components/ui-components/ProfileCardContent/Poll.jsx
+++ frontend/src/components/ui-components/ProfileCardContent/Poll.jsx
@@ -1,8 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import ApartmentTag from "./ApartmentTag.jsx";
 
-export default function Poll({pollData, setPollQuestion, setPollAnswers}) {
-    return (
-        <div>
-            Poll here
+export default function Poll({pollData}) {
+    const [answerRevealed, setAnswerRevealed] = useState(false);
+    let voteTotal = 0;
+    for (let i = 0; i < pollData.answers.length; i++){
+        voteTotal = voteTotal + pollData.answers[i].votes;
+    }
+    function displayResults(){
+        /*backend should be linked here to add a vote
+        userVote.votes++;
+        voteTotal++; */
+        setAnswerRevealed(prev => !prev);
+    }
+  return (
+    <div className={"w-full h-full rounded-lg border-solid border-2 border-maroon text-lg font-roboto_slab font-medium"}>
+      <div className={"flex w-full h-full flex-col"}>
+        {/*Top headers*/}
+        <p className={"flex justify-center w-full"}>
+          <span className={"text-center"}>
+            {pollData.question}
+          </span>
+        </p>
+        <div className={"flex h-0 w-[100%] border-solid border-b-[1px] border-maroon"}></div>
+
+        {/*Bottom panel with tags*/}
+        <div className={"flex w-full p-2 max-h-[80%] grow-[0] flex-col gap-1 overflow-y-scroll"} style={{
+          WebkitOverflowScrolling: 'touch',
+          '&::-webkit-scrollbar': {
+            display: 'none'
+          },
+          scrollbarWidth: 'none',
+
+          '&::-webkit-scrollbar': {
+            width: '0'
+          }
+        }}>
+        {answerRevealed ? 
+        pollData.answers.map((newAnswer) =>
+          <p className={"flex justify-center w-full mt-[1vh]"}>
+            <span className={"rounded-lg px-3 w-[97%] h-[33px] flex items-center justify-center border-solid border-2 border-maroon text-xs text-white bg-maroon"}>
+              {newAnswer.answer} {(newAnswer.votes/voteTotal)*100}%
+            </span>
+          </p>  
+        )
+        :
+        pollData.answers.map((newAnswer) =>
+        <p className={"flex justify-center w-full mt-[1vh]"}>
+          <button className={"rounded-lg px-3 w-[97%] h-[33px] flex items-center justify-center border-solid border-2 border-maroon text-xs text-white bg-maroon"} onClick={displayResults}>
+            {newAnswer.answer}
+          </button>
+        </p>)  }
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+}   
