a7cfaf422ff71972616ad5fce2bb3ca702622f9d
Adam Douiri <adamdouir@gmail.com>
2024-07-27 17:27:10 -0500

    first set of ui changes

--- frontend/src/components/ui-components/ProfileCardContent/Poll.jsx
+++ frontend/src/components/ui-components/ProfileCardContent/Poll.jsx
@@ -3,13 +3,15 @@ import ApartmentTag from "./ApartmentTag.jsx";
 
 export default function Poll({pollData, revealAnswers}) {
     const [answerRevealed, setAnswerRevealed] = useState(revealAnswers);
+    let [answersState, setAnswersState] = useState(pollData.answers);
+
     let voteTotal = 0;
     for (let i = 0; i < pollData.answers.length; i++){
         voteTotal = voteTotal + pollData.answers[i].votes;
     }
-    function displayResults(userVote){
+    function displayResults(index){
         /*backend should be linked here to add a vote */
-        userVote.votes++;
+        pollData.answers[index].votes++;
         voteTotal++;
         setAnswerRevealed(prev => !prev);
     }
@@ -37,17 +39,18 @@ export default function Poll({pollData, revealAnswers}) {
           }
         }}>
         {answerRevealed ? 
-        pollData.answers.map((newAnswer) =>
+        answersState.map((newAnswer, index) =>
           <p className={"flex justify-center w-full mt-[1vh]"}>
-            <span className={"rounded-lg px-3 w-[97%] h-[33px] flex items-center justify-center border-solid border-2 border-maroon text-xs text-white bg-maroon"}>
+            <div className={"rounded-lg w-[97%] h-[33px] flex items-center justify-between border-maroon text-xs text-white bg-maroon"}>
+                <div className={`bg-dark_maroon rounded-lg flex h-[100%] w-[${(newAnswer.votes/voteTotal*100).toFixed(1)}%]`}></div>
               {newAnswer.answer} {(newAnswer.votes/voteTotal*100).toFixed(1)}%
-            </span>
+            </div>
           </p>  
         )
         :
-        pollData.answers.map((newAnswer) =>
+        answersState.map((newAnswer, index) =>
         <p className={"flex justify-center w-full mt-[1vh]"}>
-          <button className={"rounded-lg px-3 w-[97%] h-[33px] flex items-center justify-center border-solid border-2 border-maroon text-xs text-white bg-maroon"} onClick={() => displayResults(newAnswer)}>
+          <button className={"rounded-lg px-3 w-[97%] h-[33px] flex items-center justify-center border-solid border-2 border-maroon text-xs text-white bg-maroon"} onClick={() => displayResults(index)}>
             {newAnswer.answer}
           </button>
         </p>)  }
