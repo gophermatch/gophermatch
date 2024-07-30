878ac3b107c9a70727518ff10f581f8dec995398
tnt417 <ar56058@gmail.com>
2024-07-29 19:24:54 -0500

    Changed schemas to be in line with the new backend

--- backend/src/database/schemas/u_qna.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE u_qna (
-    user_id INT,
-    question_id INT,
-    option_id INT,
-    PRIMARY KEY (user_id, question_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (question_id) REFERENCES qna_questions(question_id),
-    FOREIGN KEY (option_id) REFERENCES qna_options(option_id)
-);
