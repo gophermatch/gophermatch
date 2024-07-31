357829016830314903f45b6413cb3174be8e52cc
Whatshisname <120993368+Whatshisname303@users.noreply.github.com>
2024-07-31 12:32:10 -0500

    Add broadcaster to allow components to add save callbacks

--- frontend/src/components/ui-components/EditProfileCard.jsx
+++ frontend/src/components/ui-components/EditProfileCard.jsx
@@ -1,5 +1,6 @@
 import EditPoll from "./ProfileCardContent/EditPoll";
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
+import NameAndBio from "./ProfileCardContent/NameAndBio";
 
 /*
 interface qna {
@@ -33,23 +34,39 @@ interface profileData {
   aptOrDormData: aptData | dormData
 }
 */
-export function EditProfileCard({
-  user_id
-}) {
-  /* */
-  const pollData = {
-    question: "What is your favorite color?",
-    answers: [
-      { answer: "Red", votes: 15 },
-      { answer: "Blue", votes: 20 },
-      { answer: "Green", votes: 10 },
-      { answer: "Yellow", votes: 5 }
-    ]
-  };
-  let [editedPollData, setEditedPollData] = useState(pollData.answers);
-  const updatePollData= (newPollData) => {
-    setEditedPollData(newPollData);
-  };
+class SaveBroadcaster {
+  callbacks = [];
+
+  async fire() {
+    return Promise.all(this.callbacks.map(cb => cb()));
+  }
+
+  connect(callback) {
+    this.callbacks.push(callback);
+  }
+
+  disconnect(callback) {
+    const index = this.callbacks.indexOf(callback);
+    if (index > -1) {
+      this.callbacks.splice(index, 1);
+    }
+  }
+}
+
+
+export function EditProfileCard({user_id}) {
+  const broadcaster = useMemo(() => new SaveBroadcaster(), []);
+  const [isSaving, setIsSaving] = useState(false);
+
+  function onSaveClick() {
+    setIsSaving(true)
+    broadcaster.fire()
+      .then(() => console.log("All worked"))
+      .catch((e) => console.error("SAVE NOT SUCCESSFUL: ", e))
+      .finally(() => setIsSaving(false))
+      //TODO: switch to non-editing card and remount all components
+  }
+
   return ( // TODO
     <div className={`m-auto 2xl:w-[80rem] xl:w-[60rem] lg:w-[45rem] md:w-[30rem] sm:w-[20rem] h-screen flex items-center justify-center font-profile font-bold text-maroon_new`}>
       <div className={"w-full aspect-[1.8475] h-auto flex flex-col mb-[4vh] bg-white rounded-lg overflow-hidden"}>
@@ -61,8 +78,7 @@ export function EditProfileCard({
           <div className="flex flex-col lg:gap-[1.5rem] md:gap-[1rem] sm:gap-[0.5rem] grow">
             <div className="flex grow-[2] flex-col">
               <div className={"flex grow-[2] border-dashed border-2 border-black"}>
-                {/* Name */}
-                Name and major here
+                <NameAndBio name={"Joe"} major={"CSE"} bio={"Very cool"} broadcaster={broadcaster} />
               </div>
               <div className={"flex grow-[5] border-dashed border-2 border-maroon"}>
                 {/* Bio */}
@@ -83,12 +99,13 @@ export function EditProfileCard({
               <div className="grow-[2] flex flex-col lg:gap-[1.5rem] md:gap-[1rem] sm:gap-[0.5rem]">
                 <div className={"flex grow-[3] border-dashed border-2 border-maroon"}>
                   {/* Poll */}
-                  <EditPoll pollData={pollData} updatePollData={updatePollData} userId={16}/> 
+                  {/* <EditPoll pollData={pollData} updatePollData={updatePollData} userId={16}/>  */}
                 </div>
                 <div className={"flex grow-[1] border-dashed border-2 border-maroon"}>
                   {/* Sleep schedule */}
                   Sleep sched here
                 </div>
+                <button onClick={onSaveClick} className="absolute bottom-0 h-[40px] left-[40%] right-[40%] bg-maroon text-white">Save</button>
               </div>
             </div>
           </div>
--- frontend/src/components/ui-components/ProfileCardContent/NameAndBio.jsx
+++ frontend/src/components/ui-components/ProfileCardContent/NameAndBio.jsx
@@ -2,10 +2,23 @@ import { useEffect, useState } from "react";
 import backend from "../../../backend";
 import styles from '../../../assets/css/name.module.css';
 
-export default function NameAndBio({ name, major, bio, setBio, userID }) {
+export default function NameAndBio({ name, major, bio, broadcaster }) {
     const [userInfo, setUserInfo] = useState([]);
     const [editValues, setEditValues] = useState({});
     const [firstName, setFirstName] = useState('');
+
+    useEffect(() => {
+        if (broadcaster) {
+            const cb = () => {
+                return new Promise(() => {
+                    console.log("Saving data")
+                })
+            }
+    
+            broadcaster.connect(cb)
+            return () => broadcaster.disconnect(cb)
+        }
+    }, [])
 
     // const fetchUserInfo = async () => {
     //     try {
