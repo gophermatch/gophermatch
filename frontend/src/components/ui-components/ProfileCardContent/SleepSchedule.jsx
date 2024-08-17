2125f825c8ce09442221f9b64c02eb420d870bf2
Whatshisname303 <120993368+Whatshisname303@users.noreply.github.com>
2024-08-17 13:38:23 -0700

    Add sleep schedule back in

--- frontend/src/components/ui-components/ProfileCardContent/SleepSchedule.jsx
+++ frontend/src/components/ui-components/ProfileCardContent/SleepSchedule.jsx
@@ -1,8 +1,29 @@
+import React from "react"
+import InputRange from "react-input-range"
 
 export default function SleepSchedule({sleepSchedule, setSleepSchedule}) {
     return (
-        <div>
-            Sleep schedule here
-        </div>
+        <>
+            <span className="ml-[5vw]">Sleep Schedule</span>
+
+            <InputRange
+                draggableTrack
+                maxValue={144}
+                minValue={80}
+                value={sliderValue}
+                onChange={value => setSliderValue(value)}
+                formatLabel={() => null}
+                className={{
+                    slider: 'maroon',
+                    track: 'maroon',
+                    activeTrack: 'maroon',
+                    labelContainer: 'maroon',
+                }}
+            />
+            <div className="flex justify-between">
+                <span>{formatTime(sliderValue.min)}</span>
+                <span>{formatTime(sliderValue.max)}</span>
+            </div>
+        </>
     )
 }
