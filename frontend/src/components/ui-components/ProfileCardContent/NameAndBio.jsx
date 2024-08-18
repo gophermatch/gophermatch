5909cd12f58138f96d73523bfea71b5d637919d0
AndrewLange1 <lang1395@umn.edu>
2024-08-17 20:35:27 -0500

    Added class of and gender sign to bio

--- frontend/src/components/ui-components/ProfileCardContent/NameAndBio.jsx
+++ frontend/src/components/ui-components/ProfileCardContent/NameAndBio.jsx
@@ -1,137 +1,156 @@
 import { useEffect, useState } from "react";
 import backend from "../../../backend";
 import styles from '../../../assets/css/name.module.css';
+import male from '../../../assets/images/male-svgrepo-com.svg';
+import woman from '../../../assets/images/woman2-svgrepo-com.svg';
+import nonbinary from '../../../assets/images/gender-nonbinary-fill-svgrepo-com.svg';
 
-//TODO: add edit state, is in edit if broadcaster is not null
+// TODO: add edit state, is in edit if broadcaster is not null
 
 export default function NameAndBio({ user_id, broadcaster }) {
     const [bio, setBio] = useState('');
     const [major, setMajor] = useState('');
     const [name, setFullName] = useState('');
+    const [year, setYear] = useState('');
+    const [gender, setGender] = useState('');
 
     const majorList = [
-      'Accounting',
-      'Aerospace Engineering',
-      'Anthropology',
-      'Architecture',
-      'Biochemistry',
-      'Biology',
-      'Biomedical Engineering',
-      'Business Administration',
-      'Chemical Engineering',
-      'Chemistry',
-      'Civil Engineering',
-      'Communications',
-      'Computer Engineering',
-      'Computer Science',
-      'Economics',
-      'Education',
-      'Electrical Engineering',
-      'English',
-      'Environmental Science',
-      'Finance',
-      'Geography',
-      'Graphic Design',
-      'Health Sciences',
-      'History',
-      'Industrial Engineering',
-      'Information Technology',
-      'International Business',
-      'Journalism',
-      'Law',
-      'Marketing',
-      'Mathematics',
-      'Mechanical Engineering',
-      'Nursing',
-      'Philosophy',
-      'Physics',
-      'Political Science',
-      'Psychology',
-      'Public Health',
-      'Sociology',
-      'Statistics'
-  ];
-  
+        'Accounting',
+        'Aerospace Engineering',
+        'Anthropology',
+        'Architecture',
+        'Biochemistry',
+        'Biology',
+        'Biomedical Engineering',
+        'Business Administration',
+        'Chemical Engineering',
+        'Chemistry',
+        'Civil Engineering',
+        'Communications',
+        'Computer Engineering',
+        'Computer Science',
+        'Economics',
+        'Education',
+        'Electrical Engineering',
+        'English',
+        'Environmental Science',
+        'Finance',
+        'Geography',
+        'Graphic Design',
+        'Health Sciences',
+        'History',
+        'Industrial Engineering',
+        'Information Technology',
+        'International Business',
+        'Journalism',
+        'Law',
+        'Marketing',
+        'Mathematics',
+        'Mechanical Engineering',
+        'Nursing',
+        'Philosophy',
+        'Physics',
+        'Political Science',
+        'Psychology',
+        'Public Health',
+        'Sociology',
+        'Statistics'
+    ];
+
     useEffect(() => {
         async function fetchData() {
-          try {
-            const response = await backend.get('/profile/get-gendata', {
-              params: {
-                user_id: user_id,
-                filter: [
-                  'first_name', 'last_name', 'major', 'bio'
-                ]
-              }
-            });
+            try {
+                const response = await backend.get('/profile/get-gendata', {
+                    params: {
+                        user_id: user_id,
+                        filter: [
+                            'first_name', 'last_name', 'major', 'bio', 'graduating_year', 'gender'
+                        ]
+                    }
+                });
 
-            console.log("NAME BIO REQUESTED")
+                console.log("NAME BIO REQUESTED");
 
-            if (response.data && response.data.length > 0) {
-              const user = response.data[0];
-              console.log("User data:", user);
-              if (user) {
-                setFullName(`${user.first_name} ${user.last_name}`);
-                setMajor(user.major);
-                setBio(user.bio);
-              }
+                if (response.data && response.data.length > 0) {
+                    const user = response.data[0];
+                    console.log("User data:", user);
+                    if (user) {
+                        setFullName(`${user.first_name} ${user.last_name}`);
+                        setMajor(user.major);
+                        setBio(user.bio);
+
+                        // Extract the last two digits of the graduating year
+                        const yearString = user.graduating_year.toString();
+                        const lastTwoDigits = yearString.slice(-2);
+
+                        setYear(lastTwoDigits); // Set the formatted year
+                        setGender(user.gender); // Set the gender
+                    }
+                }
+            } catch (error) {
+                console.error('Error fetching user name, major, and bio:', error);
             }
-          } catch (error) {
-            console.error('Error fetching user name, major, and bio:', error);
-          }
         }
 
         fetchData();
-      }, [user_id]);
-    
+    }, [user_id]);
+
     useEffect(() => {
         if (broadcaster) {
             const cb = () =>
-              backend.post('/profile/set-gendata', {
-                user_id: user_id,
-                data: {
-                  bio: bio,
-                  major: major
-                }
-              });
+                backend.post('/profile/set-gendata', {
+                    user_id: user_id,
+                    data: {
+                        bio: bio,
+                        major: major,
+                        graduating_year: `20${year}`, // Format the year as needed
+                        gender: gender, // Use the correct field name
+                    }
+                });
 
-            broadcaster.connect(cb)
-            return () => broadcaster.disconnect(cb)
+            broadcaster.connect(cb);
+            return () => broadcaster.disconnect(cb);
         }
-    }, [broadcaster, bio, major])
-
-    // we should probably be using tailwind rather than css modules but doesn't really matter
+    }, [broadcaster, bio, major, year, gender]); // Include `gender` in dependency array
 
     return (
         <div className={styles.container}>
-            <div className="text-[3vh] font-bold">
-                {name}
+            <div className="text-[3vh] font-bold flex items-center">
+                {name} 
+                {gender === 'male' ? (
+                  <img src={male} alt="Male" className="ml-2" />
+              ) : gender === 'female' ? (
+                  <img src={woman} alt="Female" className="ml-2" />
+              ) : gender === 'nonbinary' ? (
+                  <img src={nonbinary} alt="Non-Binary" className="ml-2" />
+              ) : null}
             </div>
             <div className="mt-[-1vh] text-[2vh] font-[450]">
-              {broadcaster ? 
-                <select
-                  id="selectionBox"
-                  value={major}
-                  onChange={(e) => setMajor(e.target.value)}
-                  className="border-2 rounded-lg mt-[1vh]"
-                >
-                  {majorList.map((option, index) => (
-                    <option key={index} value={option}>
-                      {option}
-                    </option>
-                  ))}
-                </select> 
-              : major}
+                {broadcaster ?
+                    <select
+                        id="selectionBox"
+                        value={major}
+                        onChange={(e) => setMajor(e.target.value)}
+                        className="border-2 rounded-lg mt-[1vh]"
+                    >
+                        {majorList.map((option, index) => (
+                            <option key={index} value={option}>
+                                {option}
+                            </option>
+                        ))}
+                    </select>
+                    : major} {year} 
             </div>
             <div className="w-full h-[12vh] rounded-lg mt-1.5 border border-maroon flex">
-              <p className="flex-1 text-[1.8vh] text-left font-normal">
-                  {broadcaster ? 
-                    <textarea
-                      className = "w-full h-full p-2 border-none rounded-lg resize-none"
-                      value={bio}
-                      maxLength="200"
-                      onChange={(e) => setBio(e.target.value)}
-                    /> 
-                  : bio}
+                <p className="flex-1 text-[1.8vh] text-left font-normal">
+                    {broadcaster ?
+                        <textarea
+                            className="w-full h-full p-2 border-none rounded-lg resize-none"
+                            value={bio}
+                            maxLength="200"
+                            onChange={(e) => setBio(e.target.value)}
+                        />
+                        : bio}
                 </p>
             </div>
         </div>
