b85ab4204955c6b8c62f7d8fcde00f33598a0397
quuanng <quang.pham0824@gmail.com>
2024-08-18 11:37:58 +0700

    profile lock new

--- backend/src/database/schemas/users.sql
+++ backend/src/database/schemas/users.sql
@@ -2,6 +2,5 @@ CREATE TABLE users (
     user_id INT AUTO_INCREMENT,
     email VARCHAR(50) NOT NULL UNIQUE,
     hashpass VARCHAR(100) NOT NULL,
-    state ENUM('No base data', 'No profile data', 'Complete') NOT NULL DEFAULT 'No base data',
     PRIMARY KEY (user_id)
 );
