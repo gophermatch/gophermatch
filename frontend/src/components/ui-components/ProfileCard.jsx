c68304edc67501498bb42d7cfbf64b010c02d7ff
AndrewLange1 <lang1395@umn.edu>
2024-08-20 10:57:50 -0500

    Fixed profilecard.jsx scaling

--- frontend/src/components/ui-components/ProfileCard.jsx
+++ frontend/src/components/ui-components/ProfileCard.jsx
@@ -12,7 +12,7 @@ import SleepSchedule from './ProfileCardContent/SleepSchedule';
 // user_id: number, isDorm: boolean, showApt: boolean, broadcaster?: SignalBroadcaster
 export function ProfileCard({user_id, isDorm, switchProfileMode, broadcaster, dormToggle, profileMode, save_func, isDormBackend}) {
   return ( // TODO
-    <div className={`m-auto 2xl:w-[80rem] xl:w-[60rem] lg:w-[45rem] md:w-[30rem] sm:w-[20rem] h-screen flex items-center justify-center flex-col font-profile font-bold text-maroon_new relative z-10`}>
+    <div className={`m-auto 2xl:w-[80rem] xl:w-[60rem] lg:w-[45rem] md:w-[35rem] sm:w-[30rem] h-screen flex items-center justify-center flex-col font-profile font-bold text-maroon_new relative z-10`}>
       {profileMode &&
       <div className={"flex mr-[35vw]"}>
         <div className={`flex flex-row-reverse font-roboto_slab text-white w-[12vw] h-[4vh] justify-center items-center rounded-t-[1vw] ${
@@ -53,17 +53,17 @@ export function ProfileCard({user_id, isDorm, switchProfileMode, broadcaster, do
                       className="w-[4%] h-[7%] absolute top-[5%] right-[3%] bg-maroon_new rounded-full hover:bg-maroon_dark shadow-md">
                   <img src="assets/images/match-save.svg" alt="Save" className="w-[50%] h-[50%] m-auto" />
               </button>}
-        <div className={"flex p-[4vh] h-full w-full gap-[3.6%]"}>
-          <div className="w-[30vh] h-full min-w-[25%]">
+        <div className={"flex p-[2%] h-full w-full gap-[3.6%]"}>
+          <div className="w-[25%] h-full min-w-[25%]">
           <Carousel user_id={user_id} editable={broadcaster ? true : false}></Carousel>
           </div>
-          <div className="flex flex-col gap-[3.6%] grow">
+          <div className="flex flex-col w-[70.5%] gap-[3.6%]">
             <div>
               <NameAndBio user_id={user_id} broadcaster={broadcaster} />
             </div>
-            <div className="flex grow-[3] gap-[3.6%]">
-              <div className="grow-[2] flex flex-col overflow-x-hidden max-w-[60%] gap-[3.6%]">
-                <div className={"flex grow-[5] border-none border-2 border-maroon overflow-y-auto overflow-x-hidden max-h-40"}>
+            <div className="flex h-[50%] gap-[3.6%]">
+              <div className="flex w-[55%] flex-col overflow-x-hidden gap-[7.2%]">
+                <div className={"flex h-[65%] border-none overflow-y-auto overflow-x-hidden"}>
                   {/*How should we really pass this? Match and Profile page will end up passing it differently, but we could use the same prop*/}
                   {isDorm ?
                     <Top5Dorms user_id={user_id} broadcaster={broadcaster} />
@@ -71,15 +71,15 @@ export function ProfileCard({user_id, isDorm, switchProfileMode, broadcaster, do
                     <ApartmentInfo user_id={user_id} broadcaster={broadcaster} />
                   }
                 </div>
-                  <div className={"flex grow-[3]"}>
+                  <div className={"h-[40%]"}>
                     <Qna user_id={user_id} broadcaster={broadcaster} />
                   </div>
                 </div>
               <div className="grow-[2] flex flex-col gap-[3.6%]">
-                <div className={"flex grow-[3] border-none border-2 border-maroon"}>
+                <div className={"flex h-[60%] border-none border-2 border-maroon"}>
                   <Poll answersRevealed={profileMode} user_id={user_id} broadcaster={broadcaster} />
                 </div>
-                <div className={"flex grow-[1] border-dashed border-2 border-maroon"}>
+                <div className={"flex h-[35%] border-dashed border-2 border-maroon"}>
                   <SleepSchedule user_id={user_id} broadcaster={broadcaster} />
                 </div>
               </div>
