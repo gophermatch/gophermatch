759c23fc4a1acf9296659a738352c67928820ffe
tnt417 <ar56058@gmail.com>
2024-08-23 17:08:19 -0500

    New filter arrow based on new figma

--- frontend/src/components/ui-components/SubleaseFilter.jsx
+++ frontend/src/components/ui-components/SubleaseFilter.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import currentUser from '../../currentUser';
 import backend from '../../backend';
 import qnaOptions from './qnaOptions.json';
+import dropdownArrow from '../../assets/images/filterArrow.svg';
 
 export default function SubleaseFilter({ filterSetter }) {
   const [shouldShowUI, setShowUI] = useState(false);
@@ -111,43 +112,10 @@ export default function SubleaseFilter({ filterSetter }) {
         </div>
       </div>
 
-      <div className="flex justify-center z-0">
-        <svg
-          onClick={expandFilterUI}
-          className={`absolute duration-[400ms] right-[70vh] ml-auto mr-auto cursor-pointer`}
-          viewBox="0 -4.5 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          style={{
-            transform: isOpen ? "rotateX(180deg) translateY(-5.5vh)" : "rotateX(0deg) translateY(0)",
-            transition: "transform 0.45s ease", // Smooth rotation and translation
-            width: "10%", // Ensure the SVG scales correctly within the container
-            height: "10%",
-            transformStyle: "preserve-3d", // Enable 3D transformation
-            opacity: isOpen ? 1 : 0.5, // Adjust opacity based on the `isOpen` state
-          }}
-        >
-          <g
-            id="Page-1"
-            stroke="none"
-            stroke-width="0.5"
-            fill="none"
-            fill-rule="evenodd"
-          >
-            <g
-              id="Dribbble-Light-Preview"
-              transform="translate(-220.000000, -6684.000000)"
-              fill="#000000"
-            >
-              <g id="icons" transform="translate(56.000000, 160.000000)">
-                <path
-                  d="M164.292308,6524.36583 L164.292308,6524.36583 C163.902564,6524.77071 163.902564,6525.42619 164.292308,6525.83004 L172.555873,6534.39267 C173.33636,6535.20244 174.602528,6535.20244 175.383014,6534.39267 L183.70754,6525.76791 C184.093286,6525.36716 184.098283,6524.71997 183.717533,6524.31405 C183.328789,6523.89985 182.68821,6523.89467 182.29347,6524.30266 L174.676479,6532.19636 C174.285736,6532.60124 173.653152,6532.60124 173.262409,6532.19636 L165.705379,6524.36583 C165.315635,6523.96094 164.683051,6523.96094 164.292308,6524.36583"
-                  id="arrow_down-[#338]"
-                />
-              </g>
-            </g>
-          </g>
-        </svg>
+      <div className="flex justify-center z-0 mt-[15px]">
+            <button onClick={expandFilterUI}>
+                <img className={`absolute duration-[400ms] right-[12vh] ml-auto mr-auto cursor cursor-pointer transform transition-transform ${isOpen ? `scale-y-[-1] translate-y-[50px]` : `scale-y-[1] translate-y-[0px]`}`} src={dropdownArrow}></img>
+            </button>
       </div>
     </div>
   );
