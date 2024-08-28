a8d60937d7f1e1529a353c2250c74d848b472631
quuanng <quang.pham0824@gmail.com>
2024-08-28 12:29:50 -0500

    server fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,23 +1,23 @@
-import express, { json, urlencoded } from "express"
-import session from "express-session"
-import cors from "cors"
-import { port, session as envSession } from './env.js'
-import { db } from './database/db.js'
+import express, { json, urlencoded } from "express";
+import session from "express-session";
+import cors from "cors";
+import { port, session as envSession } from './env.js';
+import { db } from './database/db.js';
 
-const server = express()
+const server = express();
 
 // Use Node's queryString as query parser
-server.set('query parser', 'simple')
+server.set('query parser', 'simple');
 
 // Request body middleware
-server.use(json())    // tell server to use parse incoming requests as json (using the json() middleware)
-server.use(urlencoded({extended:true}))
+server.use(json());    // tell server to parse incoming requests as json (using the json() middleware)
+server.use(urlencoded({ extended: true }));
 
 // Session
 const cookieAge = 1000 * 60 * 60 * 24 * 7; // 7 days
 server.use(session({
     secret: envSession.secret,
-    resave: false, // prvent race condition with parallel storing to sessions of the same user
+    resave: false, // prevent race condition with parallel storing to sessions of the same user
     saveUninitialized: true, // allow newly created session to be stored
     cookie: {
         maxAge: cookieAge
@@ -27,21 +27,27 @@ server.use(session({
 server.use(cors({
     origin: "http://localhost:8080",    // do not end the url with a slash / !!
     credentials: true
-}))  // allow cross origin requests. 
+}));  // allow cross origin requests. 
 // TODO: add whitelisted origins checker (different origins for DEV and PROD)
 
-import router from "./routes/router.js"
-server.use('/api', router)
+import router from "./routes/router.js";
+server.use('/api', router);
 
+// Start the server
+const httpServer = server.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
 
 // Handle server shutdown
 let handleShutdown = () => {
     httpServer.close(() => {
-    })
+        console.log('HTTP server closed.');
+    });
 
     db.end((err) => {
-        if (err) console.error(`Database: error when ending pooling connection: ${err.stack}`)
-    })
-}
-process.on('SIGINT', handleShutdown)
-process.on('SIGTERM', handleShutdown)
+        if (err) console.error(`Database: error when ending pooling connection: ${err.stack}`);
+    });
+};
+
+process.on('SIGINT', handleShutdown);
+process.on('SIGTERM', handleShutdown);
