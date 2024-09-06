150a09bfa239f3d4581868d73cdb9abff042a44c
AndrewLange1 <lang1395@umn.edu>
2024-09-05 23:16:20 -0500

    Fixed dropdown styling

--- frontend/src/components/ui-components/ProfileCardContent/Qna.jsx
+++ frontend/src/components/ui-components/ProfileCardContent/Qna.jsx
@@ -1,151 +1,146 @@
 import { useEffect, useState } from "react";
 import backend from "../../../backend";
-import dropdownImg from "../../../assets/images/dropdown.png";
-import dropupImg from "../../../assets/images/dropup.png";
 
 const CHOICE_DICT = {
     "room_activity": ["Party", "Friends", "Empty"],
     "substances": ["Yes", "No"],
     "alcohol": ["Yes", "No"],
     "tidiness": ["Dirty", "Clean"],
-}
+};
 
-function OptionDropdown({value, name, dropdown, setDropdown, dispatch}) {
-    return(
-        <div className={`relative w-[60%] ${dropdown === name ? "z-20" : "z-10"} flex justify-end`}>
-            <button onClick={() => setDropdown(current => current === name ? undefined : name)}>
-                <img src={dropdown === name ? dropupImg : dropdownImg} className="w-[20px] inline-block" />
-                {value}
-            </button>
-            {dropdown === name &&
-                <div className="absolute min-w-full top-full border-2 border-solid border-maroon ">
-                {CHOICE_DICT[name].map((option) => (
-                    <button onClick={() => {dispatch(option); setDropdown(undefined)}} className="block w-full text-right px-[4px] border-t-[1px] border-solid hover:bg-maroon_transparent2 border-t-gray">
-                        {option}
-                    </button>
-                ))}
-                </div>
-            }
-        </div>
-    )
-}
-
-export default function Qna({user_id, broadcaster}) {
-    const [room_activity, setRoomActivity] = useState("")
-    const [substances, setSubstances] = useState("")
-    const [alcohol, setAlcohol] = useState("")
-    const [tidiness, setTidiness] = useState("")
-
-    const [currentDropdown, setCurrentDropdown] = useState(undefined)
+export default function Qna({ user_id, broadcaster }) {
+    const [room_activity, setRoomActivity] = useState("");
+    const [substances, setSubstances] = useState("");
+    const [alcohol, setAlcohol] = useState("");
+    const [tidiness, setTidiness] = useState("");
 
     useEffect(() => {
-        backend.get('/profile/get-gendata', {params: {
-            user_id: user_id,
-            filter: ['room_activity', 'substances', 'alcohol', 'tidiness'],
-        }}).then(res => {
-            setRoomActivity(res.data[0].room_activity)
-            setSubstances(res.data[0].substances)
-            setAlcohol(res.data[0].alcohol)
-            setTidiness(res.data[0].tidiness)
-        }).catch(console.error)
-    }, [])
+        backend
+            .get("/profile/get-gendata", {
+                params: {
+                    user_id: user_id,
+                    filter: ["room_activity", "substances", "alcohol", "tidiness"],
+                },
+            })
+            .then((res) => {
+                setRoomActivity(res.data[0].room_activity);
+                setSubstances(res.data[0].substances);
+                setAlcohol(res.data[0].alcohol);
+                setTidiness(res.data[0].tidiness);
+            })
+            .catch(console.error);
+    }, []);
 
     useEffect(() => {
         if (broadcaster) {
-            const cb = () => backend.post('profile/set-gendata', {
-                user_id: user_id,
-                data: {room_activity, substances, alcohol, tidiness},
-            })
+            const cb = () =>
+                backend.post("profile/set-gendata", {
+                    user_id: user_id,
+                    data: { room_activity, substances, alcohol, tidiness },
+                });
 
-            broadcaster.connect(cb)
-            return () => broadcaster.disconnect(cb)
+            broadcaster.connect(cb);
+            return () => broadcaster.disconnect(cb);
         }
-    }, [broadcaster, room_activity, substances, alcohol])
+    }, [broadcaster, room_activity, substances, alcohol, tidiness]);
 
     return (
-    <div className={"w-full h-full rounded-lg border-solid border-2 border-maroon xl:text-lg lg: text-md md:text-sm sm:text-xs font-roboto_slab font-medium"}>
-    <div className={"flex w-full h-full justify-center items-center flex-col px-[1%]"}>
-        <div className={"flex w-full whitespace-nowrap"}>
-            <div className={"flex-1"}>
-                Preferred Room Activity Level
-            </div>
-            <div className={"flex-1 text-right flex justify-end align-middle"}>
-                {broadcaster ?
-                    <OptionDropdown
-                        value={room_activity}
-                        name={"room_activity"}
-                        dropdown={currentDropdown}
-                        setDropdown={setCurrentDropdown}
-                        dispatch={setRoomActivity}
-                    />
-                    :
-                    room_activity
-                }
+        <div className="w-full h-full rounded-lg border-solid border-2 border-maroon xl:text-lg lg: text-md md:text-sm sm:text-xs font-roboto_slab font-medium">
+            <div className="flex w-full h-full justify-center items-center flex-col px-[1%]">
+                {/* Room Activity Level */}
+                <div className="flex w-full whitespace-nowrap">
+                    <div className="flex-1">Preferred Room Activity Level</div>
+                    <div className="flex-1 text-right">
+                        {broadcaster ? (
+                            <select
+                                value={room_activity}
+                                onChange={(e) => setRoomActivity(e.target.value)}
+                                className="border border-gray-300 p-1"
+                            >
+                                {CHOICE_DICT["room_activity"].map((option) => (
+                                    <option key={option} value={option}>
+                                        {option}
+                                    </option>
+                                ))}
+                            </select>
+                        ) : (
+                            room_activity
+                        )}
+                    </div>
+                </div>
+
+                <div className="flex w-[97%] h-[5%] border-b"></div>
+
+                {/* Substance Preference */}
+                <div className="flex w-full whitespace-nowrap">
+                    <div className="flex-1">Substance Preference</div>
+                    <div className="flex-1 text-right">
+                        {broadcaster ? (
+                            <select
+                                value={substances}
+                                onChange={(e) => setSubstances(e.target.value)}
+                                className="border border-gray-300 p-1"
+                            >
+                                {CHOICE_DICT["substances"].map((option) => (
+                                    <option key={option} value={option}>
+                                        {option}
+                                    </option>
+                                ))}
+                            </select>
+                        ) : (
+                            substances
+                        )}
+                    </div>
+                </div>
+
+                <div className="flex w-[97%] h-[5%] border-b"></div>
+
+                {/* Alcohol Preference */}
+                <div className="flex w-full whitespace-nowrap">
+                    <div className="flex-1">Alcohol Preference</div>
+                    <div className="flex-1 text-right">
+                        {broadcaster ? (
+                            <select
+                                value={alcohol}
+                                onChange={(e) => setAlcohol(e.target.value)}
+                                className="border border-gray-300 p-1"
+                            >
+                                {CHOICE_DICT["alcohol"].map((option) => (
+                                    <option key={option} value={option}>
+                                        {option}
+                                    </option>
+                                ))}
+                            </select>
+                        ) : (
+                            alcohol
+                        )}
+                    </div>
+                </div>
+
+                <div className="flex w-[97%] h-[5%] border-b"></div>
+
+                {/* Tidiness Preference */}
+                <div className="flex w-full whitespace-nowrap">
+                    <div className="flex-1">Preferred Tidiness</div>
+                    <div className="flex-1 text-right">
+                        {broadcaster ? (
+                            <select
+                                value={tidiness}
+                                onChange={(e) => setTidiness(e.target.value)}
+                                className="border border-gray-300 p-1"
+                            >
+                                {CHOICE_DICT["tidiness"].map((option) => (
+                                    <option key={option} value={option}>
+                                        {option}
+                                    </option>
+                                ))}
+                            </select>
+                        ) : (
+                            tidiness
+                        )}
+                    </div>
+                </div>
             </div>
         </div>
-
-        <div className={"flex w-[97%] h-[5%] border-b"}></div>
-
-        <div className={"flex w-full whitespace-nowrap"}>
-            <div className={"flex-1"}>
-                Substance Preference
-            </div>
-            <div className={"flex-1 text-right flex justify-end align-middle"}>
-                {broadcaster ?
-                    <OptionDropdown
-                        value={substances}
-                        name={"substances"}
-                        dropdown={currentDropdown}
-                        setDropdown={setCurrentDropdown}
-                        dispatch={setSubstances}
-                    />
-                    :
-                    substances
-                }
-            </div>
-        </div>
-
-        <div className={"flex w-[97%] h-[5%] border-b"}></div>
-
-        <div className={"flex w-full whitespace-nowrap"}>
-            <div className={"flex-1"}>
-                Alcohol Preference
-            </div>
-            <div className={"flex-1 text-right flex justify-end align-middle"}>
-                {broadcaster ?
-                    <OptionDropdown
-                        value={alcohol}
-                        name={"alcohol"}
-                        dropdown={currentDropdown}
-                        setDropdown={setCurrentDropdown}
-                        dispatch={setAlcohol}
-                    />
-                    :
-                    alcohol
-                }
-            </div>
-        </div>
-
-        <div className={"flex w-[97%] h-[5%] border-b"}></div>
-
-        <div className={"flex w-full whitespace-nowrap"}>
-            <div className={"flex-1"}>
-                Preferred Tidiness
-            </div>
-            <div className={"flex-1 text-right flex justify-end align-middle"}>
-                {broadcaster ?
-                    <OptionDropdown
-                        value={tidiness}
-                        name={"tidiness"}
-                        dropdown={currentDropdown}
-                        setDropdown={setCurrentDropdown}
-                        dispatch={setTidiness}
-                    />
-                    :
-                    tidiness
-                }
-            </div>
-        </div>
-    </div>
-    </div>);
+    );
 }
--- frontend/src/components/ui-components/ProfileCardContent/Top5Dorms.jsx
+++ frontend/src/components/ui-components/ProfileCardContent/Top5Dorms.jsx
@@ -23,28 +23,23 @@ const peopleDict = {
 
 export default function Top5Dorms({user_id, broadcaster}) {
 
-    //TODO: add better defaults or some other loading state
     const [top5Data, setTop5Data] = useState({
         inputs: ["Pioneer", "17th", "Frontier", "Comstock", "Middlebrook"],
-        question: "Top 5 Dorms?" //TODO: question should not exist in backend, should be hardcoded into html
+        question: "Top 5 Dorms?"
     });
 
     const [holdPos, setHoldPos] = useState(null);
     const [holdBase, setHoldBase] = useState(null);
     const [heldDorm, setHeldDorm] = useState('');
-    const [openedDropdown, setOpenedDropdown] = useState(undefined); // integer representing index
     const mousePos = useMousePosition();
 
-    const [top5Dorms, setTop5Dorms] = useState(top5Data.inputs); //TODO: this seems like duplicate state
+    const [top5Dorms, setTop5Dorms] = useState(top5Data.inputs);
     const [numPeople, setNumPeople] = useState(1);
 
-    const unusedOptions = DORM_OPTIONS.filter(option => !top5Dorms.includes(option));
-
-    function swapDormOption(i, value) {
-        const dorms = [...top5Dorms]
-        dorms[i] = value
-        setTop5Dorms(dorms)
-        setOpenedDropdown(undefined)
+    function updateDormOption(i, value) {
+        const dorms = [...top5Dorms];
+        dorms[i] = value;
+        setTop5Dorms(dorms);
     }
 
     useEffect(() => {
@@ -62,79 +57,53 @@ export default function Top5Dorms({user_id, broadcaster}) {
                     }),
                     backend.post('profile/set-gendata', {
                         user_id: user_id,
-                        data: {num_residents: numPeople}
+                        data: { num_residents: numPeople }
                     })
-                ])
-            }
+                ]);
+            };
 
-            broadcaster.connect(cb)
-            return () => broadcaster.disconnect(cb)
+            broadcaster.connect(cb);
+            return () => broadcaster.disconnect(cb);
         }
-      }, [broadcaster, top5Dorms, numPeople])
+    }, [broadcaster, top5Dorms, numPeople]);
 
     useEffect(() => {
         async function fetchData() {
-          backend.get('/profile/get-gendata', {params: {
-            user_id: user_id,
-            filter: ['num_residents']
-          }}).then(res => setNumPeople(res.data[0].num_residents));
-          try {
+            backend.get('/profile/get-gendata', {params: {
+                user_id: user_id,
+                filter: ['num_residents']
+            }}).then(res => setNumPeople(res.data[0].num_residents));
+            try {
+                const topfive = await backend.get('/profile/get-topfive', {
+                    params: { user_id: user_id }
+                });
 
-            const topfive = await backend.get('/profile/get-topfive', {
-              params: {
-                user_id: user_id
-              }
-            });
-
-            if (topfive.data) {
-              setTop5Data({
-                question: topfive.data.question,
-                inputs: [
-                    topfive.data.input1,
-                    topfive.data.input2,
-                    topfive.data.input3,
-                    topfive.data.input4,
-                    topfive.data.input5,
-                ]
-              });
-              setTop5Dorms([
-                topfive.data.input1,
-                topfive.data.input2,
-                topfive.data.input3,
-                topfive.data.input4,
-                topfive.data.input5
-              ])
+                if (topfive.data) {
+                    setTop5Data({
+                        question: topfive.data.question,
+                        inputs: [
+                            topfive.data.input1,
+                            topfive.data.input2,
+                            topfive.data.input3,
+                            topfive.data.input4,
+                            topfive.data.input5,
+                        ]
+                    });
+                    setTop5Dorms([
+                        topfive.data.input1,
+                        topfive.data.input2,
+                        topfive.data.input3,
+                        topfive.data.input4,
+                        topfive.data.input5
+                    ]);
+                }
+            } catch (error) {
+                console.error('Error fetching user profile:', error);
             }
-          } catch (error) {
-            console.error('Error fetching user profile:', error);
-          }
         }
 
         fetchData();
-      }, [user_id]);
-
-    const offset = holdPos ? mousePos.y - holdPos.y : undefined;
-
-    useEffect(() => {
-        if (!holdPos) {
-            return;
-        }
-        const dormIndex = top5Dorms.findIndex(dorm => dorm === heldDorm);
-
-        if (offset < -35 && dormIndex > 0) { // raised up a position
-            [top5Dorms[dormIndex], top5Dorms[dormIndex - 1]] = [top5Dorms[dormIndex - 1], top5Dorms[dormIndex]];
-            setTop5Dorms([...top5Dorms]);
-            setHoldBase(v => v + offset);
-            setHoldPos(mousePos);
-        }
-
-        if (offset > 35 && dormIndex < top5Dorms.length - 1) { // lowered down a position
-            [top5Dorms[dormIndex], top5Dorms[dormIndex + 1]] = [top5Dorms[dormIndex + 1], top5Dorms[dormIndex]];
-            setTop5Dorms([...top5Dorms]);
-            setHoldBase(v => v + offset);
-            setHoldPos(mousePos);
-        }
-    }, [mousePos]);
+    }, [user_id]);
 
     return (
         <div className="flex flex-col border-2 border-solid border-maroon_new rounded-md w-full h-full p-[5px] font-roboto_slab custom-scrollbar">
@@ -144,11 +113,10 @@ export default function Top5Dorms({user_id, broadcaster}) {
                 </div>
                 {broadcaster ?
                     <button onClick={() => setNumPeople(num => (num % 4) + 1)} className="bg-maroon text-white px-1 rounded-full">-&gt;</button>
-                    :
-                    <></>
+                    : <></>
                 }
             </div>
-            <hr className="border-t-1 bordet-top-solid border-maroon_new"></hr>
+            <hr className="border-t-1 border-top-solid border-maroon_new"></hr>
             <div className="flex-1 overflow-auto custom-scrollbar pr-[5px]">
                 <p className="text-sm">{top5Data.question}</p>
                 <div className="relative">
@@ -160,47 +128,36 @@ export default function Top5Dorms({user_id, broadcaster}) {
                                 key={dorm}
                                 className={`bg-maroon w-[calc(100%-30px)] leading-[30px] pl-[5px] rounded-md text-white select-none absolute flex align-middle ${!isHeld && 'transition-all'}`}
                                 style={{
-                                    top: isHeld ? holdBase + offset : basePosition,
+                                    top: isHeld ? holdBase + mousePos.y - holdPos?.y : basePosition,
                                     left: "30px",
-                                    zIndex: heldDorm === dorm || openedDropdown === i ? 1 : 0,
+                                    zIndex: heldDorm === dorm ? 1 : 0,
                                 }}
                                 onMouseLeave={() => { setHoldPos(null); setHoldBase(null); setHeldDorm(''); }}
                                 onMouseUp={() => { setHoldPos(null); setHoldBase(null); setHeldDorm(''); }}
                             >
-                                {broadcaster ?
+                                {broadcaster ? (
                                     <>
-                                    <div
-                                        className="h-[30px] w-[18px] flex align-middle cursor-grab"
-                                        onMouseDown={() => { setHoldPos(mousePos); setHoldBase(basePosition); setHeldDorm(dorm); }}
+                                        <div
+                                            className="h-[30px] w-[18px] flex align-middle cursor-grab"
+                                            onMouseDown={() => { setHoldPos(mousePos); setHoldBase(basePosition); setHeldDorm(dorm); }}
                                         >
-                                        <img src={hamburger} draggable="false" />
-                                    </div>
-                                    <div
-                                        className="border-[1px] border-white border-solid pl-[4px] m-[4px] leading-[22px] h-[22px] w-[50%] cursor-pointer relative"
-                                        onClick={openedDropdown !== i ? () => setOpenedDropdown(i): () => {}}
-                                    >
-                                        {openedDropdown === i ?
-                                            <div className="absolute top-0 right-0 left-0 bg-white text-maroon border-[1px] border-solid border-inactive_gray">
-                                                <button className="block w-full text-left hover:bg-offwhite" onClick={() => setOpenedDropdown(undefined)}>{dorm}</button>
-                                                {unusedOptions.map(option => (
-                                                    <button
-                                                        key={option}
-                                                        className="block w-full text-left hover:bg-offwhite border-t-inactive_gray border-t-[1px] border-t-solid"
-                                                        onClick={() => swapDormOption(i, option)}
-                                                        >
-                                                        {option}
-                                                    </button>
-                                                ))}
-                                            </div>
-                                            :
-                                            dorm
-                                        }
-
-                                    </div>
+                                            <img src={hamburger} draggable="false" alt="drag icon" />
+                                        </div>
+                                        <select
+                                            className="border-[1px] bg-maroon_new pl-[4px] m-[4px] leading-[22px] h-[22px] w-[50%] cursor-pointer"
+                                            value={dorm}
+                                            onChange={(e) => updateDormOption(i, e.target.value)}
+                                        >
+                                            {DORM_OPTIONS.map(option => (
+                                                <option key={option} value={option}>
+                                                    {option}
+                                                </option>
+                                            ))}
+                                        </select>
                                     </>
-                                    :
+                                ) : (
                                     dorm
-                                }
+                                )}
                             </div>
                         );
                     })}
@@ -218,7 +175,7 @@ export default function Top5Dorms({user_id, broadcaster}) {
 const useMousePosition = () => {
     const [mousePosition, setMousePosition] = useState({ x: null, y: null });
     useEffect(() => {
-        const updateMousePosition = ev => {
+        const updateMousePosition = (ev) => {
             setMousePosition({ x: ev.clientX, y: ev.clientY });
         };
         window.addEventListener('mousemove', updateMousePosition);
